acf6f15caadf54c9738e8636cc97a32e383b89e8
Vitalik Vakulenko <121180497+vakulenko10@users.noreply.github.com>
2026-03-02 19:30:43 +0100

    Revert "added chat and debugged it a bit"
    This reverts commit 95080619d472e3ffef8a72f5dde037a3baf33027.

--- app/components/AutoUIChatClient.tsx
+++ app/components/AutoUIChatClient.tsx
@@ -1,19 +1,10 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import type { AutoUIConfig } from "@autoai-ui/autoui";
+import { ModalChat } from "@autoai-ui/autoui";
 import autouiConfig from "../autouiConfig";
 
-const ModalChatNoSSR = dynamic<{ config: AutoUIConfig }>(
-  () =>
-    import("@autoai-ui/autoui").then((mod) => {
-      return mod.ModalChat;
-    }),
-  { ssr: false }
-);
-
 const AutoUIChatClient = () => {
-  return <ModalChatNoSSR config={autouiConfig} />;
+  return <ModalChat config={autouiConfig} />;
 };
 
 export default AutoUIChatClient;
